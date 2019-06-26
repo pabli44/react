857c4d87a0da55a07d90b47c1bc03d8c87dc5707
Pablo Velilla <pabli44@hotmail.com>
2019-06-25 21:33:58 -0500

    Sorting icon, decoupling components

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
+
 
 
 class MoviesTable extends Component {
@@ -37,17 +37,12 @@ class MoviesTable extends Component {
         const { movies, onSort, sortColumn} = this.props;
 
         return (
-            <table className="table">
-                <TableHeader 
-                    columns={this.columns} 
-                    sortColumn={sortColumn} 
-                    onSort={onSort}
-                />
-                <TableBody 
-                    data={movies}
-                    columns ={this.columns}
-                />
-            </table>  
+            <Table 
+                columns={this.columns} 
+                data={movies} 
+                sortColumn={sortColumn} 
+                onSort={onSort}
+            />
         )
     }
 }
