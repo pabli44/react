5242eec57602b8418eddfa94ffdbab8fe2c2c857
Pablo Gabriel Velilla Goenaga <pablo.velilla@sophos.col.com>
2019-10-22 13:45:41 -0500

    updates

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Switch>
           <Route path="/products/:id" component={ProductDetail}/>
           <Route path="/products" render={props => <Products sortBy="newest" {...props} />} />
-          <Route path="/posts" component={Posts}/>
+          <Route path="/posts/:year?/:month?" component={Posts}/>
           <Route path="/admin" component={DashBoard}/>
           <Route path="/" component={Home}/>
         </Switch>
--- router-app/src/components/products.jsx
+++ router-app/src/components/products.jsx
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Products extends Component {
-    
+    state = {
+        products:[
+            {id: 1, name: "Product 1"},
+            {id: 2, name: "Product 2"},
+            {id: 3, name: "Product 3"}
+        ]
+    }
+
+
     render() { 
         const {sortBy} = this.props;
         console.log(sortBy);
         
-        return (  
-            <p>Products</p>
+       
+            
 
+        return (  
+            <div>
+                <h1>Products</h1>
+                <ul>
+                    {this.state.products.map(product => (
+                        <li key={product.id}>
+                            <Link to={`/products/${product.id}`}>{product.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         );
     }
 }
