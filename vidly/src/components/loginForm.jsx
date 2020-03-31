a8a4e5302a10eeba1624e246266e0f0bd1cc05c6
pabli44 <pablivg@gmail.com>
2020-03-30 23:09:46 -0500

    Back again in the course

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+    state = {
+        account: { username:'', password: '' }
+    };
 
     handleSubmit = e =>{
         e.preventDefault();
@@ -8,18 +11,40 @@ class LoginForm extends Component {
         console.log("Submitted");
     }
 
+    handleChange = ({ currentTarget: input }) =>{
+        const account = {...this.state.account};
+        account[input.name] = input.value;
+        this.setState({ account });
+    };
+
     render() { 
+        const { account } = this.state;
+
         return ( 
             <div>
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username: </label>
-                        <input id="username" type="text" className="form-control" />
+                        <input 
+                            value={account.username} 
+                            onChange={this.handleChange}
+                            name="username"
+                            id="username" 
+                            type="text" 
+                            className="form-control" 
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password: </label>
-                        <input id="password" type="text" className="form-control" />
+                        <input 
+                            value={account.password} 
+                            onChange={this.handleChange}
+                            name="password"
+                            id="password" 
+                            type="text" 
+                            className="form-control" 
+                        />
                     </div>
                 </form>
             </div>
