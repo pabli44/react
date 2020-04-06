ce3a08d6a2aebd20f3f443e67e82bcd5ef8d7b92
pabli44 <pablivg@gmail.com>
2020-04-05 23:20:13 -0500

    new lessons 59% react course, using Joi with validations by schema

--- vidly/src/components/common/navbar.jsx
+++ vidly/src/components/common/navbar.jsx
@@ -5,19 +5,19 @@ class NavBar extends Component {
     state = {  }
     render() { 
         return (  
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item mr-3">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item mr-3">
                         <Link to="/movies">Movies</Link>
                     </li>
                 </ul>
-                <ul class="navbar-nav">
-                    <li class="nav-item mr-3">
+                <ul className="navbar-nav">
+                    <li className="nav-item mr-3">
                     <Link to="/login">Login</Link>
                     </li>
                 </ul>
--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,51 +1,87 @@
 import React, { Component } from 'react';
+import Joi from 'joi-browser';
+import Input from '../components/common/input';
 
 class LoginForm extends Component {
     state = {
-        account: { username:'', password: '' }
+        account: { username:'', password: '' },
+        errors: {}
     };
+
+
+	schema = {
+		username: Joi.string().required().label('Username'),
+		password: Joi.string().required().label('Password')		
+	}
+
+    validate = () =>{
+		const options = { abortEarly:false }
+		const { error } = Joi.validate(this.state.account, this.schema, options);
+
+		if(!error) return null;
+
+		const errors = {};
+		for (let item of error.details)
+			errors[item.path[0]] = item.message;
+
+        return errors;
+    }
 
     handleSubmit = e =>{
         e.preventDefault();
 
-        console.log("Submitted");
+        const errors = this.validate();
+        this.setState({ errors: errors || {} });
+        if(errors) return;
+
+        this.doSubmit();
+	}
+	
+	doSubmit = () =>{
+		console.log("Submitted");
+	}
+
+    validateProperty = ({ name, value }) => {
+	   const obj = { [name]: value };
+	   const schema = { [name]: this.schema[name] };
+	   const { error } = Joi.validate(obj, schema);
+	   
+	   return error ? error.details[0].message:null;
     }
 
     handleChange = ({ currentTarget: input }) =>{
+        const errors = {...this.state.errors};
+        const errorMessage = this.validateProperty(input);
+        if(errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
         const account = {...this.state.account};
         account[input.name] = input.value;
-        this.setState({ account });
+        this.setState({ account,errors });
     };
 
     render() { 
-        const { account } = this.state;
+        const { account, errors } = this.state;
 
         return ( 
             <div>
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="username">Username: </label>
-                        <input 
-                            value={account.username} 
-                            onChange={this.handleChange}
-                            name="username"
-                            id="username" 
-                            type="text" 
-                            className="form-control" 
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password: </label>
-                        <input 
-                            value={account.password} 
-                            onChange={this.handleChange}
-                            name="password"
-                            id="password" 
-                            type="text" 
-                            className="form-control" 
-                        />
-                    </div>
+                   <Input 
+                        name="username" 
+                        value={account.username} 
+                        label="Username" 
+                        error={errors.username} 
+                        onChange={this.handleChange}
+                    />
+                    <Input 
+                        name="password" 
+                        value={account.password} 
+                        label="Password" 
+                        error={errors.password}
+                        onChange={this.handleChange} 
+                    />
+                    <button className="btn btn-primary"  disabled={this.validate()}>Login</button>
                 </form>
             </div>
          );
